d5b50dbdbf5287e51921131f12708ed78d9d940f
Andrew Gerber <andrewj.gerber@gmail.com>
2023-06-07 21:26:07 -0600

    Force

--- add_service.sh
+++ add_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -s /home/pi/splash/splashcam.service /etc/systemd/system/splashcam.service
+sudo ln -sf /home/pi/splash/splashcam.service /etc/systemd/system/splashcam.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable splashcam
